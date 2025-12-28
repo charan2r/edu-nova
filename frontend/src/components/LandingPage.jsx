02b2f68a353529c25de13a94929718ff8d9fc2ae
Charan Romi <s92075569@ousl.lk>
2025-12-28 19:40:51 +0530

    added CI/CD

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -60,24 +60,11 @@ const LandingPage = () => {
           sx={{
             marginTop: "100px",
             padding: "10px 30px",
-            backgroundColor: "#1976d2",
+            backgroundColor: "#5490ccff",
             color: "#fff",
             fontWeight: "bold",
             fontSize: "1.2rem",
             borderRadius: 3,
-            boxShadow: `
-                0 4px 24px 0 rgba(49, 66, 134, 0.25),
-                0 0 12px 2px rgb(67, 79, 187),
-                0 2px 8px 0 rgba(0,0,0,0.18)
-    `,
-            "&:hover": {
-              backgroundColor: "#154fa6",
-              boxShadow: `
-                0 8px 32px 0 rgba(25,30,50,0.35),
-                0 0 18px 4px rgb(30, 73, 138),
-                0 4px 16px 0 rgba(0,0,0,0.22)
-      `,
-            },
           }}
           onClick={handleGetStarted}
         >
