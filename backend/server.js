3fbc441d15d9c37fec2c8bad6a6e66c08f5a5779
Charan Romi <s92075569@ousl.lk>
2026-05-30 13:58:21 +0530

    minor fixes

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const { generalLimiter } = require("./middleware/rateLimiter");
 const app = express();
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "user-id"],
